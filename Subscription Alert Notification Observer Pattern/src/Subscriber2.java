6e2446bcaf48002523f9db5cce9a630988065145
sandeep-alluri <89863764+sandeep-alluri@users.noreply.github.com>
2021-09-05 09:28:25 -0700

    Delete Subscription Alert Notification Observer Pattern directory

--- Subscription Alert Notification Observer Pattern/src/Subscriber2.java
+++
@@ -1,9 +0,0 @@
-
-public class Subscriber2 implements Observer{
-
-	@Override
-	public void update(Message message) {
-		System.out.println("Subscriber2   :: " + message.getMessageContent());
-		
-	} 
-}
--- Subscription Alert Notification Observer Pattern/src/Subscriber3.java
+++
@@ -1,9 +0,0 @@
-
-public class Subscriber3 implements Observer{
-
-	@Override
-	public void update(Message message) {
-		System.out.println("Subscriber3  :: " + message.getMessageContent());
-		
-	} 
-}
